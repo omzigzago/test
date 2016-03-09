355605f9ee559db4c3543caf6df1b13554bb189e
omzigzago <zigzag.demba@gmail.com>
2016-03-09 15:22:25 +0100

    Create toto.php

---
+++ toto.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Document sans nom</title>
+</head>
+
+<body>
+<form action="titi.php" method="post">
+<input type="submit" value="ok" />
+</form>
+</body>
+</html>
